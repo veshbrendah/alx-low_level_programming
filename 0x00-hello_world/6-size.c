f466478575b74375b046182bd0d978afe4c83334
your name <you@example.com>
2022-06-02 15:37:59 -0700

    seven fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int intType;
 long int long_int;
 long long int longType;
 float floatType;
-printf("size of a char: %ld byte(s)\n", sizeof(charType));
-printf("size of an int: %ld byte(s)\n", sizeof(intType));
-printf("size of a long int: %ld byte(s)\n", sizeof(long_int));
-printf("size of a long long int: %ld byte(s)\n", sizeof(longType));
-printf("size of a float: %ld byte(s)\n", sizeof(floatType));
+printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+printf("Size of an int: %ld byte(s)\n", sizeof(intType));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long_int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(longType));
+printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 return (0);
 }
