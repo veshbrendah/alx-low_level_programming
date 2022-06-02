37b11f4fcb0fd9ab8ded82f42717f95d8dd1a956
your name <you@example.com>
2022-06-02 15:29:12 -0700

    seven

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ long long int longType;
 float floatType;
 printf("size of a char: %ld byte(s)\n", sizeof(charType));
 printf("size of an int: %ld byte(s)\n", sizeof(intType));
-printf("size of a long int: %ld byte(s)", sizeof(long_int));
+printf("size of a long int: %ld byte(s)\n", sizeof(long_int));
 printf("size of a long long int: %ld byte(s)\n", sizeof(longType));
 printf("size of a float: %ld byte(s)\n", sizeof(floatType));
 return (0);
