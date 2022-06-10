0964945afe476bf9c05579002f76209cceb75bb1
your name <you@example.com>
2022-06-10 06:39:20 -0700

    print squares

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,7 +3,6 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_triangle(int size);
-void print_square(int size);
 int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
