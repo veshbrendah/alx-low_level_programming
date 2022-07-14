58d9fb9f2c3ede6ba5195e5904e42f5829641fc2
your name <you@example.com>
2022-07-14 06:23:49 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 
 /**
-  * print_listint - prints all elements of a list
-  *
-  * @h: list to print elements of
-  * Return: size_t of the list
-  */
-size_t print_listint(const listint_t *h)
+ * list_len - Calculate the number of elements.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ **/
+
+size_t list_len(const list_t *h)
 {
-	size_t n = 0;
+	const list_t *temp;
+	unsigned int counter = 0;
 
-	while (h)
+	temp = h;
+	while (temp)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
-		n++;
+		counter++;
+		temp = temp->next;
 	}
-	return (n);
+	return (counter);
 }
-
